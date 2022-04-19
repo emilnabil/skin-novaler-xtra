c225d93de9828336023323dae14b30663de1f273
emil-nabil333 <elbrinselomda@yahoo.com>
2022-04-19 18:53:53 +0200

    Add files via upload

--- installer.sh
+++ installer.sh
@@ -1,11 +1,12 @@
 #!/bin/sh 
 # 
- # # Command: wget https://raw.githubusercontent.com/emilnabil/skin-novaler-xtra/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
-MY_URL="https://raw.githubusercontent.com/emilnabil/skin-novaler-xtra/main"  
-NOVALLER-XTRA-BLUE="Novaler-S-Xtra-BLUE.tar.gz"
-NOVALER-XTRA-COOLER="Novaler-S-Xtra-Cooler.tar.gz"
-NOVALER-XTRA-MUSTARD="Novaler-S-Xtra-Mustard.tar.gz"
-NOVALER-XTRA-TWILLIGHT="Novaler-S-Xtra-Twillight.tar.gz"
+## Command: wget https://raw.githubusercontent.com/emilnabil/skin-novaler-xtra/main/installer.sh -qO - | /bin/sh # # ########################################### ###########################################  
+MY_URL="https://raw.githubusercontent.com/emilnabil/skin-novaler-xtra/main"
+########################################  
+NOVALLER_XTRA_BLUE="Novaler-S-Xtra-BLUE.tar.gz"
+NOVALER_XTRA_COOLER="Novaler-S-Xtra-Cooler.tar.gz"
+NOVALER_XTRA_MUSTARD="Novaler-S-Xtra-Mustard.tar.gz"
+NOVALER_XTRA_TWILLIGHT="Novaler-S-Xtra-Twillight.tar.gz"
 echo "******************************************************************************************************************"
 echo "    Download And Install Skins Novaler-Xtra  "
 echo "============================================================================================================================="
@@ -13,31 +14,31 @@ echo "==========================================================================
 echo "         INSTALL SKIN-NOVALLER-XTRA-BLUE    "
 cd /tmp
 set -e 
-wget "$MY_URL/$NOVALLER-XTRA-BLUE"
+wget "$MY_URL/$NOVALLER_XTRA_BLUE"
 wait
-tar -xzf $NOVALLER-XTRA-BLUE  -C /
+tar -xzf $NOVALLER_XTRA_BLUE  -C /
 wait
 cd ..
 set +e
-rm -f /tmp/$NOVALLER-XTRA-BLUE
+rm -f /tmp/$NOVALLER_XTRA_BLUE
 echo "==========================================================================================================================="
 echo "         INSTALL SKIN-NOVALER-XTRA-COOLER    "
 cd /tmp
 set -e 
-wget "$MY_URL/$NOVALER-XTRA-COOLER"
+wget "$MY_URL/$NOVALER_XTRA_COOLER"
 wait
-tar -xzf $NOVALER-XTRA-COOLER  -C /
+tar -xzf $NOVALER_XTRA_COOLER  -C /
 wait
 cd ..
 set +e
-rm -f /tmp/$NOVALER-XTRA-COOLER
+rm -f /tmp/$NOVALER_XTRA_COOLER
 echo "****************************************************************************************************************************"
 echo "       INSTALL  SKIN NOVALER-XTRA-MUSTARD    "
 cd /tmp
 set -e 
-wget "$MY_URL/$NOVALER-XTRA-MUSTARD"
+wget "$MY_URL/$NOVALER_XTRA_MUSTARD"
 wait
-tar -xzf $NOVALER-XTRA-MUSTARD  -C /
+tar -xzf $NOVALER_XTRA_MUSTARD  -C /
 wait
 cd ..
 set +e
@@ -46,13 +47,13 @@ echo "**************************************************************************
 echo "         Install SKIN-NOVALER-XTRA-TWILLIGHT    "
 cd /tmp
 set -e 
-wget "$MY_URL/$NOVALER-XTRA-TWILLIGHT"
+wget "$MY_URL/$NOVALER_XTRA_TWILLIGHT"
 wait
-tar -xzf $NOVALER-XTRA-TWILLIGHT  -C /
+tar -xzf $NOVALER_XTRA_TWILLIGHT  -C /
 wait
 cd ..
 set +e
-rm -f /tmp/$NOVALER-XTRA-TWILLIGHT
+rm -f /tmp/$NOVALER_XTRA_TWILLIGHT
 echo "==========================================================================================================================="
 echo "==========================================================================================================================="
 echo "# SKIN -S-XTRA NOVALER INSTALLED #"
@@ -68,6 +69,7 @@ echo "**************************************************************************
 wait
 killall -9 enigma2
 exit 0
+
 
 
 
